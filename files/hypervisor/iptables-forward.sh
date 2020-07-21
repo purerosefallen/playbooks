fbc4e96d6e648ac1573fdf4b5fc0c58f29fed1b3
purerosefallen <78877@qq.com>
2020-07-21 09:15:37 +0800

    multiport

--- files/hypervisor/iptables-forward.sh
+++ files/hypervisor/iptables-forward.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -p tcp -m tcp --dport $1 -j DNAT --to-destination $2
-sudo iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -p udp -m udp --dport $1 -j DNAT --to-destination $2
+sudo iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -p tcp -m multiport --dports $1 -j DNAT --to-destination $2
+sudo iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -p udp -m multiport --dports $1 -j DNAT --to-destination $2
 cp ./ipt ./ipt.bak
 sudo iptables-save > ./ipt
 #netfilter-persistent save 
